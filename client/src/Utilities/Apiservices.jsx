f26cbd273588f8cf3078c538d4c587d7edb4cd7d
Prakash-V-S <prakashvs.official@gmail.com>
2025-04-19 21:11:06 +0530

    render push

--- client/src/Utilities/Apiservices.jsx
+++ client/src/Utilities/Apiservices.jsx
@@ -1,9 +1,13 @@
 import axios from "axios"
+
 //temp mail revaw48202@huizk.com
 
 // const API_KEY = 'https://65d858a0c96fbb24c1bb4db4.mockapi.io'
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api' ;
+
+// render app
+const API_BASE_URL = 'https://ashokleyland-lcv.onrender.com' ;
 
 export const axiosService = axios.create({
     baseURL:API_BASE_URL,
