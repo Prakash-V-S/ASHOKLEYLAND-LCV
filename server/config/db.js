5f90e6d8e0e04694e9f1579e908ec906eb6f4e4b
Prakash-V-S <prakashvs.official@gmail.com>
2025-04-19 21:30:14 +0530

    db config

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async (app) => {
       useUnifiedTopology: true,
     });
 
-    const PORT = 5000;
+    const PORT = process.env.PORT || 5000;
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
   } catch (error) {
