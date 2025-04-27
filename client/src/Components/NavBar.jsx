a5cf0213300c880a1cab2f9843ae68f72d88da37
Prakash-V-S <prakashvs.official@gmail.com>
2025-04-27 19:54:58 +0530

    nav bar changes

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,40 +1,32 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { NavLink } from 'react-router-dom';
-import ashokleylandLogo from '../assets/ASHOKLEY.NS-ccd24b88.png'
-
+import { NavLink } from "react-router-dom";
+import ashokleylandLogo from "../assets/ASHOKLEY.NS-ccd24b88.png";
 
 function NavBar() {
   return (
-    <Navbar expand="md" bg="dark" variant="dark" className="bg-gray-800 text-red-300"
-    style={{}}>
-      <Container className="mx-4">
-      <Navbar.Brand href="#home">
-            <img
-              src={ashokleylandLogo}
-              width="50"
-              height="2000"
-              className="d-inline-block align-top "
-              alt="React Bootstrap logo"
-            />
-            {/* <img
-              src={'src/assets/55853709-stack-of-colored-books-the-logo-of-the-bookstore-the-book-icon-bookstore-or-library-the-book-on-the-removebg-preview (1).png'}
-              width="50"
-              height="50"
-              className="d-inline-block align-top "
-              alt="React Bootstrap logo"
-            /> */}
-          </Navbar.Brand>
-        <Navbar.Brand href="#"  className="text-blue-200">Customer Testimonial</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar expand="md" bg="dark" variant="dark" className="bg-gray-800">
+      <Container fluid className="px-4">
+        <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center gap-2">
+          <img
+            src={ashokleylandLogo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="Ashok Leyland Logo"
+          />
+          <span className="text-blue-200">Customer Testimonial</span>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
+
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav1">
-          <Nav.Link as={NavLink} to="/"  className="text-yellow-400"> 
+          <Nav className="ms-auto text-yellow-400">
+            <Nav.Link as={NavLink} to="/" className="text-yellow-400">
               Home
             </Nav.Link>
-          <Nav.Link as={NavLink} to="/dashboard"  className="text-yellow-400">
+            <Nav.Link as={NavLink} to="/dashboard" className="text-yellow-400">
               Dashboard
             </Nav.Link>
             <Nav.Link as={NavLink} to="/create" className="text-yellow-400">
